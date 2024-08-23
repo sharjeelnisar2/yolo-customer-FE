5d20a1ae868401fe6d046c5d89f33201d7993c61
Muhammad Hassan Ikram <bcsf20m525@pucit.edu.pk>
2024-08-23 16:34:27 +0500

    Directory structure and basic UI layout

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import GenerateIdea from '@/views/GenerateIdea.vue';
+import ViewIdeas from '@/views/ViewIdeas.vue';
+import ViewOrders from '@/views/ViewOrders.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/generate-idea', name: 'GenerateIdea', component: GenerateIdea },
+  { path: '/view-ideas', name: 'ViewIdeas', component: ViewIdeas },
+  { path: '/view-orders', name: 'ViewOrders', component: ViewOrders }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
